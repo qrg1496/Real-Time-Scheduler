35c7d00c850653c89a1075398e9ce6fffd20034c
qrg1496 <qrg1496@rit.edu>
2015-09-20 18:02:23 -0400

    Initial writting of LST and EDF need to adjust how program holds and records runtime

--- Scheduler.c
+++ Scheduler.c
@@ -13,9 +13,9 @@
 #include <sys/netmgr.h>
 
 
-#define STATE_IDLE 0;
-#define STATE_READY 1;
-#define STATE_RUNNING 2;
+#define STATE_IDLE 0
+#define STATE_READY 1
+#define STATE_RUNNING 2
 
 static pthread_t scheduleThreadID;
 pthread_mutex_t mutex;
@@ -27,10 +27,11 @@ typedef struct{
 		pthread_t threadID;
 		pthread_mutex_t mutex;
 		int state;
+		//int slackTime; //Time to deadline when program complete
 } ProgramInfo;
 
-//ProgramInfo * programsArray;
-static int numPrograms = 1;
+ProgramInfo * programsArray;
+static int numPrograms = 0;
 pthread_cond_t control;
 
 int pid;
@@ -40,16 +41,19 @@ int pulse_id = 0;
 
 
 void * program(void * arg){
-	//int location = (int)&arg[0];
-	ProgramInfo * program = (ProgramInfo *)arg;
-	int time = program->runTime;
+	int location = (int)&arg[0];
+	//ProgramInfo * program = (ProgramInfo *)arg;
+	int time = programsArray[location].runTime;
 
 	while(1){
-		pthread_mutex_lock(&(program->mutex));
-		program->state = STATE_RUNNING;
-		nanospin_ns(time * 1000000);
+		//pthread_mutex_lock(&(programsArray[location].mutex));
+		if(programsArray[location].state == STATE_READY){
+			programsArray[location].state = STATE_RUNNING;
+			nanospin_ns(time * 1000000);
 
-		program->state = STATE_IDLE;
+			//programsArray[location].slackTime = programsArray[location].deadline;
+			programsArray[location].state = STATE_IDLE;
+		}
 
 	}
 }
@@ -57,7 +61,7 @@ void * program(void * arg){
 void * rateMonotonicScheduler(void * arg){
 
 	//int progNumbers = (int)&arg[0];
-	ProgramInfo * programsArray = (ProgramInfo *)arg;
+	//ProgramInfo * programsArray = (ProgramInfo *)arg;
 	pthread_attr_t progThreadAttributes;
 	struct sched_param progParameters;
 	int policy;
@@ -83,72 +87,146 @@ void * rateMonotonicScheduler(void * arg){
 		progParameters.sched_priority--;
 
 		pthread_attr_setschedparam(&progThreadAttributes, &progParameters);
-		pthread_create(&(programsArray[loopCounter].threadID), &progThreadAttributes, &program, (void *)(programsArray + loopCounter));
+		pthread_create(&(programsArray[loopCounter].threadID), &progThreadAttributes, &program, (void *)loopCounter);
 	}
 
 	for(;;){
 			pid= MsgReceivePulse(chid, &pulse, sizeof(pulse),NULL);
-			for(loopCounter = 1; loopCounter < numPrograms; loopCounter++){
-				if(programsArray[loopCounter].deadline != 0 && programsArray[loopCounter].state == 2)
+			for(loopCounter = 0; loopCounter < numPrograms; loopCounter++){
+				if(programsArray[loopCounter].deadline != 0)
 					{
 					programsArray[loopCounter].deadline--;
 					}
 				if(programsArray[loopCounter].deadline == 0){
-					if(programsArray[loopCounter].state == 0){
-						programsArray[loopCounter].deadline=programsArray[loopCounter].period;
-						programsArray[loopCounter].state = 1;
-						pthread_mutex_unlock(&(programsArray[loopCounter].mutex));
-					}
-					else{
+					if(programsArray[loopCounter].state == STATE_RUNNING || programsArray[loopCounter].state == STATE_READY){
 						perror("Program Failure Detected\n");
 						printf("Program %i Failed to meet Deadline\n",loopCounter);
 						exit(0);
 					}
+					if(programsArray[loopCounter].state == STATE_IDLE){
+						programsArray[loopCounter].deadline=programsArray[loopCounter].period;
+						programsArray[loopCounter].state = STATE_READY;
+						//pthread_mutex_unlock(&(programsArray[loopCounter].mutex));
+					}
+
 				}
 			}
 
 		}
+	printf("If you reached here, the scheduler stopped running\n");
 
 }
 
-/*void * earliestDeadlineScheduler(void * arg){
+void * earliestDeadlineScheduler(void * arg){
+	pthread_attr_t progThreadAttributes;
+	struct sched_param progParameters;
+	int policy;
+
+	pthread_attr_init(&progThreadAttributes);
+	pthread_getschedparam(pthread_self(),&policy, &progParameters);
+
+	int shortest = 500;
+	int nextShortest = 500;
+	int found = 1;
+	int threadNum=0;
+
+	int loopCounter;
+ 	ProgramInfo buffer;
+	int back;
+	for(loopCounter = 1; loopCounter < numPrograms; loopCounter++){
+		buffer= programsArray[loopCounter];
+		back = loopCounter-1;
+		while(back >= 0 && (programsArray[loopCounter].deadline < buffer.deadline)){
+			programsArray[back+1]=programsArray[back];
+			back--;
+		}
+		programsArray[back+1]=buffer;
+
+	}
+
+	for(loopCounter = 0; loopCounter < numPrograms; loopCounter++){
+		progParameters.sched_priority--;
+
+		pthread_attr_setschedparam(&progThreadAttributes, &progParameters);
+		pthread_create(&(programsArray[loopCounter].threadID), &progThreadAttributes, &program, (void *)loopCounter);
+	}
+
+	for(;;){
+		pid= MsgReceivePulse(chid, &pulse, sizeof(pulse),NULL);
+
+
+			/*shortest
+			 *next shortest
+			 *first pass record shortest, give it highest priority
+			 *next pass find next shortest, give it 1 lower priority, then make it shortest
+			 *repeat until next shotest not found
+			 *
+			 */
+			pthread_getschedparam(pthread_self(),&policy, &progParameters);
+
+			for(loopCounter = 0; loopCounter < numPrograms; loopCounter++){
+				if(programsArray[loopCounter].deadline  < shortest){
+					shortest = programsArray[loopCounter].deadline;
+					threadNum=loopCounter;
+
+				}
+			}
+			pthread_setschedprio(programsArray[threadNum].threadID, progParameters.sched_priority--);
+			loopCounter = 0;
+			while(found){
+				found = 0;
+				for(loopCounter = 0;loopCounter < numPrograms;loopCounter++){
+					if(programsArray[loopCounter].deadline < nextShortest && programsArray[loopCounter].deadline > shortest){
+						nextShortest = programsArray[loopCounter].deadline;
+						found = 1;
+						threadNum = loopCounter;
+					}
+				}
+				if(found){
+					pthread_setschedprio(programsArray[threadNum].threadID, progParameters.sched_priority--);
+				}
+			}
+
+
+	}
 
 }
 
-void * leastSlackTime(void * arg){
+/*void * leastSlackTime(void * arg){
+
 
 }*/
 
-ProgramInfo* ReadData(){
+void ReadData(){
 	char buffer[11];
 	printf("Number of Programs?\n");
 	fgets(buffer,sizeof(buffer),stdin);
 	numPrograms = atoi(buffer);
-	ProgramInfo programArray[numPrograms];
+	programsArray = (ProgramInfo *)malloc(numPrograms*sizeof(ProgramInfo));
 	int i;
 	for(i = 0; i < numPrograms; i++){
 		printf("Runtime of Program %i?\n", i);
 		fgets(buffer,sizeof(buffer),stdin);
-		programArray[i].runTime=atoi(buffer);
+		programsArray[i].runTime=atoi(buffer);
 
 		printf("Period of Program %i?\n",i);
 		fgets(buffer,sizeof(buffer),stdin);
-		programArray[i].period=atoi(buffer);
+		programsArray[i].period=atoi(buffer);
 
 		printf("Deadline of Program %i?\n",i);
 		fgets(buffer,sizeof(buffer),stdin);
-		programArray[i].deadline=atoi(buffer);
+		programsArray[i].deadline=atoi(buffer);
 
-		if(programArray[i].period != programArray[i].deadline){
+		if(programsArray[i].period != programsArray[i].deadline){
 			perror("Deadline Not Equal to Period!\n");
 			exit(EXIT_FAILURE);
 		}
 
-		programArray[i].state=STATE_READY;
-		pthread_mutex_init(&(programArray[i].mutex),NULL);
+		programsArray[i].state=STATE_READY;
+		pthread_mutex_init(&(programsArray[i].mutex),NULL);
+		pthread_mutex_unlock(&(programsArray[i].mutex));
 	}
 
-	return programArray;
 }
 
 int main(int argc, char *argv[]) {
@@ -166,14 +244,14 @@ int main(int argc, char *argv[]) {
 
 
 	parameters.sched_priority--;
-
+	pthread_attr_setdetachstate(&threadAttributes, PTHREAD_CREATE_JOINABLE);
 	pthread_attr_setschedparam(&threadAttributes, &parameters);
 
 	pthread_mutex_init(&mutex,NULL);
 	nanospin_calibrate(0);
 
 	/*Real Time Clock Setup*/
-	clkper.nsec = 10000000;
+	clkper.nsec = 1000000;
 	clkper.fract = 0;
 
 	ClockPeriod(CLOCK_REALTIME, &clkper, NULL, 0);
@@ -192,19 +270,23 @@ int main(int argc, char *argv[]) {
 	timer_create( CLOCK_REALTIME, &event, &timer_id );
 
 	timer.it_value.tv_sec=0;
-	timer.it_value.tv_nsec=10000000;
+	timer.it_value.tv_nsec=1000000;
 	timer.it_interval.tv_sec=0;
-	timer.it_interval.tv_nsec=10000000;
+	timer.it_interval.tv_nsec=1000000;
 
 	timer_settime( timer_id, 0, &timer, NULL );
 
-	ProgramInfo * programArray = ReadData();
+	ReadData();
 
 	pthread_cond_init(&control, NULL);
 
 
 
-	pthread_create(&scheduleThreadID,&threadAttributes, &rateMonotonicScheduler, (void *)programArray);
+	pthread_create(&scheduleThreadID,&threadAttributes, &rateMonotonicScheduler, (void *)1);
+	pthread_join(scheduleThreadID,NULL);
+	while(1){
+
+	}
 
 
 
